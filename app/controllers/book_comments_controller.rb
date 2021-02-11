5e3543c65e5fa0f605960fd47be052b1e2c76af4
EC2 Default User <ec2-user@ip-172-31-95-40.ec2.internal>
2021-02-11 09:51:21 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,27 +1,34 @@
 class BookCommentsController < ApplicationController
-	before_action :authenticate_user!
-
-	def create
-		@book = Book.find(params[:book_id])
-		@book_comment = BookComment.new(book_comment_params)
-		@book_comment.book_id = @book.id
-		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
-		end
-	end
-
-	def destroy
-		@book = Book.find(params[:book_id])
-  	book_comment = @book.book_comments.find(params[:id])
-		book_comment.destroy
-		redirect_to request.referer
-	end
-
-	private
-	def book_comment_params
-		params.require(:book_comment).permit(:comment)
-	end
+    before_action :authenticate_user!
+    before_action :ensure_correct_user, only: [:destroy]
+    
+    def create
+        @book = Book.find(params[:book_id])
+        @book_comment = current_user.book_comments.new(book_id: params[:book_id], comment: params[:comment])
+        @book_comment.save
+        respond_to do |format|
+        format.html {redirect_back(fallback_location: root_path)}
+        format.js
+        end
+    end
+    
+    def destroy
+        @book_comment = BookComment.find(params[:id])
+        @book = @book_comment.book
+        @book_comment.destroy
+        respond_to do |format|
+        format.html {redirect_back(fallback_location: root_path)}
+        format.js
+        end
+    end
+    
+    private
+    
+    def ensure_correct_user
+    @book_comment = BookComment.find(params[:id])
+        unless @book_comment.user == current_user
+        redirect_to book_path(@book_comment.book)
+        end
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   
   resources :books do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   
